b22c97f40f38f9303f112dff270218f178b78a93
Lennart Ochel <lennart.ochel@liu.se>
2018-06-11 23:57:37 +0200

    Add test case for oms2_setLoggingInterval

--- OMSimulator/DualMassOscillator_me.lua
+++ OMSimulator/DualMassOscillator_me.lua
@@ -1,5 +1,5 @@
 -- status: correct
--- teardown_command: rm DualMassOscillator_me.log DualMassOscillator_me.mat
+-- teardown_command: rm DualMassOscillator_me.log DualMassOscillator_me1.mat DualMassOscillator_me2.mat
 
 oms2_setLogFile("DualMassOscillator_me.log")
 oms2_setTempDirectory("./DualMassOscillator_me_tmp")
@@ -14,8 +14,7 @@ oms2_addConnection("DualMassOscillator", "System1:F", "System2:F")
 oms2_addConnection("DualMassOscillator", "System1:s", "System2:s")
 oms2_addConnection("DualMassOscillator", "System1:v", "System2:v")
 oms2_addConnection("DualMassOscillator", "System1:a", "System2:a")
-
-oms2_setResultFile("DualMassOscillator", "DualMassOscillator_me.mat")
+oms2_setResultFile("DualMassOscillator", "DualMassOscillator_me1.mat")
 
 oms2_setStopTime("DualMassOscillator", 0.1)
 oms2_setCommunicationInterval("DualMassOscillator", 1e-5)
@@ -30,7 +29,8 @@ for _,var in ipairs(vars) do
 end
 
 oms2_reset("DualMassOscillator")
-
+oms2_setResultFile("DualMassOscillator", "DualMassOscillator_me2.mat")
+oms2_setLoggingInterval("DualMassOscillator", 1e-3)
 oms2_setStopTime("DualMassOscillator", 0.2)
 oms2_initialize("DualMassOscillator")
 oms2_simulate("DualMassOscillator")
