c7362e52b6631793381ff31b6a17c93848370e52
Adeel Asghar <adeel.asghar@liu.se>
2018-11-02 11:53:34 +0100

    Test `oms3_deleteConnectorFromBus` and `oms3_deleteConnectorFromTLMBus`

--- oms3/buses.lua
+++ oms3/buses.lua
@@ -50,6 +50,9 @@ printStatus(status, 0)
 status = oms3_addConnectorToBus("model.tlm.wc1.bus1","model.tlm.wc2.y1")
 printStatus(status, 3)
 
+status = oms3_addConnectorToBus("model.tlm.wc1.bus1","model.tlm.wc1.y")
+printStatus(status, 0)
+
 status = oms3_addBus("model.tlm.wc2.bus2")
 printStatus(status, 0)
 
@@ -74,6 +77,12 @@ printStatus(status, 3)
 src, status = oms3_list("model.tlm")
 print(src)
 
+status = oms3_deleteConnectorFromBus("model.tlm.wc1.bus1","model.tlm.wc1.y")
+printStatus(status, 0)
+
+src, status = oms3_list("model.tlm")
+print(src)
+
 status = oms3_delete("model")
 printStatus(status, 0)
 
@@ -86,8 +95,9 @@ printStatus(status, 0)
 -- status:  [correct] ok
 -- status:  [correct] ok
 -- status:  [correct] ok
--- error:   [addConnectorToBus] Connector and bus must belong to the same system
+-- error:   [addConnectorToBus] Bus and connector must belong to same system
 -- status:  [correct] error
+-- status:  [correct] ok
 -- status:  [correct] ok
 -- status:  [correct] ok
 -- status:  [correct] ok
@@ -96,6 +106,77 @@ printStatus(status, 0)
 -- status:  [correct] ok
 -- error:   [addConnection] Connection <"wc2.bus2", "wc1.bus1"> exists already in system "model.tlm"
 -- status:  [correct] error
+-- <?xml version="1.0"?>
+-- <ssd:System name="tlm">
+-- 	<ssd:SimulationInformation>
+-- 		<ssd:Annotations>
+-- 			<ssd:Annotation type="org.openmodelica">
+-- 				<oms:TlmMaster ip="" managerport="0" monitorport="0" />
+-- 			</ssd:Annotation>
+-- 		</ssd:Annotations>
+-- 	</ssd:SimulationInformation>
+-- 	<ssd:Elements>
+-- 		<ssd:System name="wc2">
+-- 			<ssd:SimulationInformation>
+-- 				<FixedStepMaster description="oms-ma" stepSize="0.100000" />
+-- 			</ssd:SimulationInformation>
+-- 			<ssd:Elements />
+-- 			<ssd:Connectors>
+-- 				<ssd:Connector name="y1" kind="output" type="Real" />
+-- 				<ssd:Connector name="y2" kind="output" type="Real" />
+-- 				<ssd:Connector name="y3" kind="output" type="Real" />
+-- 			</ssd:Connectors>
+-- 			<ssd:Connections />
+-- 			<ssd:Annotations>
+-- 				<ssd:Annotation type="org.openmodelica">
+-- 					<oms:Bus name="bus2">
+-- 						<oms:Signals>
+-- 							<oms:Signal name="y1" />
+-- 							<oms:Signal name="y2" />
+-- 						</oms:Signals>
+-- 					</oms:Bus>
+-- 				</ssd:Annotation>
+-- 			</ssd:Annotations>
+-- 		</ssd:System>
+-- 		<ssd:System name="wc1">
+-- 			<ssd:SimulationInformation>
+-- 				<FixedStepMaster description="oms-ma" stepSize="0.100000" />
+-- 			</ssd:SimulationInformation>
+-- 			<ssd:Elements />
+-- 			<ssd:Connectors>
+-- 				<ssd:Connector name="u1" kind="input" type="Real" />
+-- 				<ssd:Connector name="u2" kind="input" type="Real" />
+-- 				<ssd:Connector name="y" kind="output" type="Real" />
+-- 			</ssd:Connectors>
+-- 			<ssd:Connections />
+-- 			<ssd:Annotations>
+-- 				<ssd:Annotation type="org.openmodelica">
+-- 					<oms:Bus name="bus1">
+-- 						<oms:Signals>
+-- 							<oms:Signal name="u1" />
+-- 							<oms:Signal name="u2" />
+-- 							<oms:Signal name="y" />
+-- 						</oms:Signals>
+-- 					</oms:Bus>
+-- 				</ssd:Annotation>
+-- 			</ssd:Annotations>
+-- 		</ssd:System>
+-- 	</ssd:Elements>
+-- 	<ssd:Connectors />
+-- 	<ssd:Connections>
+-- 		<ssd:Connection startElement="wc2" startConnector="y1" endElement="wc1" endConnector="u1" />
+-- 		<ssd:Connection startElement="wc2" startConnector="y2" endElement="wc1" endConnector="u2" />
+-- 	</ssd:Connections>
+-- 	<ssd:Annotations>
+-- 		<ssd:Annotation type="org.openmodelica">
+-- 			<oms:Connections>
+-- 				<oms:Connection startElement="wc1" startConnector="bus1" endElement="wc2" endConnector="bus2" />
+-- 			</oms:Connections>
+-- 		</ssd:Annotation>
+-- 	</ssd:Annotations>
+-- </ssd:System>
+-- 
+-- status:  [correct] ok
 -- <?xml version="1.0"?>
 -- <ssd:System name="tlm">
 -- 	<ssd:SimulationInformation>
@@ -164,7 +245,7 @@ printStatus(status, 0)
 -- 		</ssd:Annotation>
 -- 	</ssd:Annotations>
 -- </ssd:System>
---
+-- 
 -- status:  [correct] ok
 -- info:    0 warnings
 -- info:    2 errors
--- oms3/tlmbuses.lua
+++ oms3/tlmbuses.lua
@@ -79,6 +79,12 @@ printStatus(status, 0)
 src, status = oms3_list("model.tlm")
 print(src)
 
+status = oms3_deleteConnectorFromTLMBus("model.tlm.wc1.bus2","model.tlm.wc1.x")
+printStatus(status, 0)
+
+src, status = oms3_list("model.tlm")
+print(src)
+
 status = oms3_delete("model")
 printStatus(status, 0)
 
@@ -89,7 +95,7 @@ printStatus(status, 0)
 -- info:    New model "model" with corresponding temp directory <suppressed>
 -- status:  [correct] ok
 -- status:  [correct] ok
--- error:   [addConnectorToTLMBus] Connector not found in system: z
+-- error:   [addConnectorToTLMBus] Connector "z" not found in system "model.tlm.wc1"
 -- status:  [correct] error
 -- error:   [addConnector] Unknown TLM variable type: effort
 -- status:  [correct] error
@@ -172,7 +178,80 @@ printStatus(status, 0)
 -- 		</ssd:Annotation>
 -- 	</ssd:Annotations>
 -- </ssd:System>
---
+-- 
+-- status:  [correct] ok
+-- <?xml version="1.0"?>
+-- <ssd:System name="tlm">
+-- 	<ssd:SimulationInformation>
+-- 		<ssd:Annotations>
+-- 			<ssd:Annotation type="org.openmodelica">
+-- 				<oms:TlmMaster ip="127.0.1.1" managerport="11111" monitorport="11121" />
+-- 			</ssd:Annotation>
+-- 		</ssd:Annotations>
+-- 	</ssd:SimulationInformation>
+-- 	<ssd:Elements>
+-- 		<ssd:System name="wc2">
+-- 			<ssd:SimulationInformation>
+-- 				<FixedStepMaster description="oms-ma" stepSize="0.100000" />
+-- 			</ssd:SimulationInformation>
+-- 			<ssd:Elements />
+-- 			<ssd:Connectors>
+-- 				<ssd:Connector name="y" kind="input" type="Real" />
+-- 				<ssd:Connector name="x" kind="output" type="Real" />
+-- 				<ssd:Connector name="v" kind="output" type="Real" />
+-- 				<ssd:Connector name="f" kind="input" type="Real" />
+-- 			</ssd:Connectors>
+-- 			<ssd:Connections />
+-- 			<ssd:Annotations>
+-- 				<ssd:Annotation type="org.openmodelica">
+-- 					<oms:Bus name="bus2" type="tlm" domain="output" dimensions="1" interpolation="none">
+-- 						<oms:Signals>
+-- 							<oms:Signal name="y" type="value" />
+-- 						</oms:Signals>
+-- 					</oms:Bus>
+-- 				</ssd:Annotation>
+-- 			</ssd:Annotations>
+-- 		</ssd:System>
+-- 		<ssd:System name="wc1">
+-- 			<ssd:SimulationInformation>
+-- 				<FixedStepMaster description="oms-ma" stepSize="0.100000" />
+-- 			</ssd:SimulationInformation>
+-- 			<ssd:Elements />
+-- 			<ssd:Connectors>
+-- 				<ssd:Connector name="y" kind="output" type="Real" />
+-- 				<ssd:Connector name="x" kind="output" type="Real" />
+-- 				<ssd:Connector name="v" kind="output" type="Real" />
+-- 				<ssd:Connector name="f" kind="input" type="Real" />
+-- 			</ssd:Connectors>
+-- 			<ssd:Connections />
+-- 			<ssd:Annotations>
+-- 				<ssd:Annotation type="org.openmodelica">
+-- 					<oms:Bus name="bus1" type="tlm" domain="input" dimensions="1" interpolation="none">
+-- 						<oms:Signals>
+-- 							<oms:Signal name="y" type="value" />
+-- 						</oms:Signals>
+-- 					</oms:Bus>
+-- 					<oms:Bus name="bus2" type="tlm" domain="mechanical" dimensions="1" interpolation="none">
+-- 						<oms:Signals>
+-- 							<oms:Signal name="f" type="effort" />
+-- 							<oms:Signal name="v" type="flow" />
+-- 						</oms:Signals>
+-- 					</oms:Bus>
+-- 				</ssd:Annotation>
+-- 			</ssd:Annotations>
+-- 		</ssd:System>
+-- 	</ssd:Elements>
+-- 	<ssd:Connectors />
+-- 	<ssd:Connections />
+-- 	<ssd:Annotations>
+-- 		<ssd:Annotation type="org.openmodelica">
+-- 			<oms:Connections>
+-- 				<oms:Connection startElement="wc1" startConnector="bus1" endElement="wc2" endConnector="bus2" delay="0.001000" alpha="0.300000" linearimpedance="100.000000" angularimpedance="0.000000" />
+-- 			</oms:Connections>
+-- 		</ssd:Annotation>
+-- 	</ssd:Annotations>
+-- </ssd:System>
+-- 
 -- status:  [correct] ok
 -- info:    0 warnings
 -- info:    2 errors
