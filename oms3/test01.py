041e4ac6a62289e9d8d4ca010cd5ce0b5deabe3d
arun3688 <rain100falls@gmail.com>
2018-11-09 13:34:14 +0100

    add oms3 python tests
      - OpenModelica/OMSimulator#481
      - OpenModelica/OMSimulator-testsuite#119

---
+++ oms3/test01.py
@@ -0,0 +1,166 @@
+## status: correct
+## teardown_command:
+## linux: yes
+## mingw: yes
+## win: yes
+## mac: yes
+
+from OMSimulator import OMSimulator
+session=OMSimulator()
+
+session.oms3_setCommandLineOption("--suppressPath=true")
+
+def printStatus(status, expected):
+  cmp = ""
+  if status == expected:
+    cmp = "correct"
+  else:
+    cmp = "wrong"
+  
+  if 0 == status:
+    status = "ok"
+  elif 1 == status:
+    status = "warning"
+  elif 3 == status:
+    status = "error"
+  print("status:  [", cmp, "] ",status)
+
+
+status = session.oms3_setTempDirectory(".")
+printStatus(status, 0)
+
+status = session.oms3_newModel("test")
+printStatus(status, 0)
+
+status = session.oms3_addSystem("test", session.oms_system_tlm)
+printStatus(status, 3)
+
+status = session.oms3_addSystem("test.foo", session.oms_system_wc)
+printStatus(status, 0)
+
+status = session.oms3_addSystem("test.foo.goo", session.oms_system_sc)
+printStatus(status, 0)
+
+status = session.oms3_addSystem("test.foo.hoo", session.oms_system_wc)
+printStatus(status, 3)
+
+status, src = session.oms3_list("test")
+print(src)
+
+status, src = session.oms3_list("test.foo")
+print(src)
+
+status, src = session.oms3_list("test.foo.goo")
+print(src)
+
+status = session.oms3_newModel("test")
+printStatus(status, 3)
+
+status = session.oms3_rename("test", "foo")
+printStatus(status, 0)
+
+status = session.oms3_newModel("test")
+printStatus(status, 0)
+
+status = session.oms3_delete("test")
+printStatus(status, 0)
+
+status = session.oms3_delete("foo")
+printStatus(status, 0)
+
+status = session.oms3_newModel("test")
+printStatus(status, 0)
+
+status, src = session.oms3_list("test")
+print(src)
+
+status = session.oms3_delete("test")
+printStatus(status, 0)
+
+status = session.oms3_delete("foo")
+printStatus(status, 3)
+
+## Result:
+## info:    Set temp directory to    <suppressed>
+## info:    Set working directory to <suppressed>
+## info:    Set temp directory to    <suppressed>
+## ('status:  [', 'correct', '] ', 'ok')
+## info:    New model "test" with corresponding temp directory <suppressed>
+## ('status:  [', 'correct', '] ', 'ok')
+## error:   [addSystem] Model "test" does not contain any system
+## ('status:  [', 'correct', '] ', 'error')
+## ('status:  [', 'correct', '] ', 'ok')
+## ('status:  [', 'correct', '] ', 'ok')
+## error:   [NewSystem] A WC system must be the root system or a subsystem of a TLM system.
+## ('status:  [', 'correct', '] ', 'error')
+## <?xml version="1.0"?>
+## <ssd:SystemStructureDescription name="test" version="Draft20180219">
+## 	<ssd:System name="foo">
+## 		<ssd:SimulationInformation>
+## 			<FixedStepMaster description="oms-ma" stepSize="0.100000" />
+## 		</ssd:SimulationInformation>
+## 		<ssd:Elements>
+## 			<ssd:System name="goo">
+## 				<ssd:SimulationInformation>
+## 					<VariableStepSolver description="cvode" absoluteTolerance="0.000100" relativeTolerance="0.000100" minimumStepSize="0.000100" maximumStepSize="0.100000" initialStepSize="0.000100" />
+## 				</ssd:SimulationInformation>
+## 				<ssd:Elements />
+## 				<ssd:Connectors />
+## 				<ssd:Connections />
+## 			</ssd:System>
+## 		</ssd:Elements>
+## 		<ssd:Connectors />
+## 		<ssd:Connections />
+## 	</ssd:System>
+## 	<ssd:DefaultExperiment startTime="0.000000" stopTime="1.000000" />
+## </ssd:SystemStructureDescription>
+## 
+## <?xml version="1.0"?>
+## <ssd:System name="foo">
+## 	<ssd:SimulationInformation>
+## 		<FixedStepMaster description="oms-ma" stepSize="0.100000" />
+## 	</ssd:SimulationInformation>
+## 	<ssd:Elements>
+## 		<ssd:System name="goo">
+## 			<ssd:SimulationInformation>
+## 				<VariableStepSolver description="cvode" absoluteTolerance="0.000100" relativeTolerance="0.000100" minimumStepSize="0.000100" maximumStepSize="0.100000" initialStepSize="0.000100" />
+## 			</ssd:SimulationInformation>
+## 			<ssd:Elements />
+## 			<ssd:Connectors />
+## 			<ssd:Connections />
+## 		</ssd:System>
+## 	</ssd:Elements>
+## 	<ssd:Connectors />
+## 	<ssd:Connections />
+## </ssd:System>
+## 
+## <?xml version="1.0"?>
+## <ssd:System name="goo">
+## 	<ssd:SimulationInformation>
+## 		<VariableStepSolver description="cvode" absoluteTolerance="0.000100" relativeTolerance="0.000100" minimumStepSize="0.000100" maximumStepSize="0.100000" initialStepSize="0.000100" />
+## 	</ssd:SimulationInformation>
+## 	<ssd:Elements />
+## 	<ssd:Connectors />
+## 	<ssd:Connections />
+## </ssd:System>
+## 
+## error:   [newModel] A model "test" already exists in the scope
+## ('status:  [', 'correct', '] ', 'error')
+## ('status:  [', 'correct', '] ', 'ok')
+## info:    New model "test" with corresponding temp directory <suppressed>
+## ('status:  [', 'correct', '] ', 'ok')
+## ('status:  [', 'correct', '] ', 'ok')
+## ('status:  [', 'correct', '] ', 'ok')
+## info:    New model "test" with corresponding temp directory <suppressed>
+## ('status:  [', 'correct', '] ', 'ok')
+## <?xml version="1.0"?>
+## <ssd:SystemStructureDescription name="test" version="Draft20180219">
+## 	<ssd:DefaultExperiment startTime="0.000000" stopTime="1.000000" />
+## </ssd:SystemStructureDescription>
+## 
+## ('status:  [', 'correct', '] ', 'ok')
+## error:   [deleteModel] Model "foo" does not exist in the scope
+## ('status:  [', 'correct', '] ', 'error')
+## info:    0 warnings
+## info:    4 errors
+## endResult
