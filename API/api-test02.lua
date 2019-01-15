e7b0d5dbd74a8fdf6178130c3ea4019c845bb5b9
Lennart Ochel <lennart.ochel@liu.se>
2019-01-15 13:53:28 +0100

    Update DualMassOscillator

--- API/api-test02.lua
+++ API/api-test02.lua
@@ -10,19 +10,19 @@ status = oms2_newFMIModel("api_test02")
 print("status code: " .. status)
 
 -- add FMUs
-status = oms2_addFMU("api_test02", "../FMUs/DualMassOscillator.System1_me.fmu", "System1")
+status = oms2_addFMU("api_test02", "../FMUs/DualMassOscillator.System1.fmu", "System1")
 print("status code: " .. status)
-status = oms2_addFMU("api_test02", "../FMUs/DualMassOscillator.System2_me.fmu", "System2")
+status = oms2_addFMU("api_test02", "../FMUs/DualMassOscillator.System2.fmu", "System2")
 print("status code: " .. status)
 
 -- add connections
 status = oms2_addConnection("api_test02", "System1:F", "System2:F")
 print("status code: " .. status)
-status = oms2_addConnection("api_test02", "System1:s", "System2:s")
+status = oms2_addConnection("api_test02", "System1:s1", "System2:s1")
 print("status code: " .. status)
-status = oms2_addConnection("api_test02", "System1:v", "System2:v")
+status = oms2_addConnection("api_test02", "System1:v1", "System2:v1")
 print("status code: " .. status)
-status = oms2_addConnection("api_test02", "System1:a", "System2:a")
+status = oms2_addConnection("api_test02", "System1:a1", "System2:a1")
 print("status code: " .. status)
 
 status = oms2_saveModel("api_test02", "api_test02.xml")
@@ -36,13 +36,13 @@ print("status code: " .. status)
 status, ident = oms2_loadModel("api_test02.xml")
 print("status code: " .. status .. " (" .. ident .. ")")
 
-status = oms2_deleteConnection("api_test02", "System1:a", "System2:a")
+status = oms2_deleteConnection("api_test02", "System1:a1", "System2:a1")
 print("status code: " .. status)
-status = oms2_deleteConnection("api_test02", "System1:s", "System2:s")
+status = oms2_deleteConnection("api_test02", "System1:s1", "System2:s1")
 print("status code: " .. status)
 status = oms2_deleteConnection("api_test02", "System1:F", "System2:F")
 print("status code: " .. status)
-status = oms2_deleteConnection("api_test02", "System1:v", "System2:v")
+status = oms2_deleteConnection("api_test02", "System1:v1", "System2:v1")
 print("status code: " .. status)
 
 status, source2 = oms2_listModel("api_test02")
@@ -76,31 +76,31 @@ print(source2)
 -- <?xml version="1.0" encoding="UTF-8"?>
 -- <ssd:SystemStructureDescription name="api_test02" version="Draft20171219">
 -- 	<ssd:System name="api_test02">
--- 		<ssd:Component name="System1" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System1_me.fmu">
+-- 		<ssd:Component name="System1" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System1.fmu">
 -- 			<ssd:Connectors>
 -- 				<ssd:Connector name="F" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.500000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="a" kind="output" type="Real">
+-- 				<ssd:Connector name="a1" kind="output" type="Real">
 -- 					<ssd:ConnectorGeometry x="1.000000" y="0.250000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="s" kind="output" type="Real">
+-- 				<ssd:Connector name="s1" kind="output" type="Real">
 -- 					<ssd:ConnectorGeometry x="1.000000" y="0.500000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="v" kind="output" type="Real">
+-- 				<ssd:Connector name="v1" kind="output" type="Real">
 -- 					<ssd:ConnectorGeometry x="1.000000" y="0.750000" />
 -- 				</ssd:Connector>
 -- 			</ssd:Connectors>
 -- 		</ssd:Component>
--- 		<ssd:Component name="System2" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System2_me.fmu">
+-- 		<ssd:Component name="System2" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System2.fmu">
 -- 			<ssd:Connectors>
--- 				<ssd:Connector name="a" kind="input" type="Real">
+-- 				<ssd:Connector name="a1" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.250000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="s" kind="input" type="Real">
+-- 				<ssd:Connector name="s1" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.500000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="v" kind="input" type="Real">
+-- 				<ssd:Connector name="v1" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.750000" />
 -- 				</ssd:Connector>
 -- 				<ssd:Connector name="F" kind="output" type="Real">
@@ -110,9 +110,9 @@ print(source2)
 -- 		</ssd:Component>
 -- 		<ssd:Connections>
 -- 			<ssd:Connection startElement="System2" startConnector="F" endElement="System1" endConnector="F" />
--- 			<ssd:Connection startElement="System1" startConnector="s" endElement="System2" endConnector="s" />
--- 			<ssd:Connection startElement="System1" startConnector="v" endElement="System2" endConnector="v" />
--- 			<ssd:Connection startElement="System1" startConnector="a" endElement="System2" endConnector="a" />
+-- 			<ssd:Connection startElement="System1" startConnector="s1" endElement="System2" endConnector="s1" />
+-- 			<ssd:Connection startElement="System1" startConnector="v1" endElement="System2" endConnector="v1" />
+-- 			<ssd:Connection startElement="System1" startConnector="a1" endElement="System2" endConnector="a1" />
 -- 		</ssd:Connections>
 -- 	</ssd:System>
 -- 	<ssd:DefaultExperiment startTime="0.000000" stopTime="1.000000" />
@@ -121,31 +121,31 @@ print(source2)
 -- <?xml version="1.0" encoding="UTF-8"?>
 -- <ssd:SystemStructureDescription name="api_test02" version="Draft20171219">
 -- 	<ssd:System name="api_test02">
--- 		<ssd:Component name="System1" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System1_me.fmu">
+-- 		<ssd:Component name="System1" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System1.fmu">
 -- 			<ssd:Connectors>
 -- 				<ssd:Connector name="F" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.500000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="a" kind="output" type="Real">
+-- 				<ssd:Connector name="a1" kind="output" type="Real">
 -- 					<ssd:ConnectorGeometry x="1.000000" y="0.250000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="s" kind="output" type="Real">
+-- 				<ssd:Connector name="s1" kind="output" type="Real">
 -- 					<ssd:ConnectorGeometry x="1.000000" y="0.500000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="v" kind="output" type="Real">
+-- 				<ssd:Connector name="v1" kind="output" type="Real">
 -- 					<ssd:ConnectorGeometry x="1.000000" y="0.750000" />
 -- 				</ssd:Connector>
 -- 			</ssd:Connectors>
 -- 		</ssd:Component>
--- 		<ssd:Component name="System2" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System2_me.fmu">
+-- 		<ssd:Component name="System2" type="application/x-fmu-sharedlibrary" source="../FMUs/DualMassOscillator.System2.fmu">
 -- 			<ssd:Connectors>
--- 				<ssd:Connector name="a" kind="input" type="Real">
+-- 				<ssd:Connector name="a1" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.250000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="s" kind="input" type="Real">
+-- 				<ssd:Connector name="s1" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.500000" />
 -- 				</ssd:Connector>
--- 				<ssd:Connector name="v" kind="input" type="Real">
+-- 				<ssd:Connector name="v1" kind="input" type="Real">
 -- 					<ssd:ConnectorGeometry x="0.000000" y="0.750000" />
 -- 				</ssd:Connector>
 -- 				<ssd:Connector name="F" kind="output" type="Real">
